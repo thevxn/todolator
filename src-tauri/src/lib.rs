33ded9c6e20c383a7a0c08cf824e04deb12a6e11
Tackx <tack@vxn.dev>
2025-05-29 19:06:52 +0200

    WIP

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,28 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde_json;
+use std::fs;
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::TrayIconBuilder,
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    Manager,
 };
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct Task {
+    name: String,
+}
+
+#[tauri::command]
+fn get_tasks() -> Result<Vec<Task>, String> {
+    let data = fs::read_to_string("src/tasks.json").map_err(|e| e.to_string())?;
+    let tasks: Vec<Task> = serde_json::from_str(&data).map_err(|e| e.to_string())?;
+    println!("{:?}", tasks);
+    Ok(tasks)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -28,11 +44,27 @@ pub fn run() {
                         println!("menu item {:?} not handled", event.id);
                     }
                 })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::DoubleClick {
+                        button: MouseButton::Left,
+                        ..
+                    } => {
+                        println!("double left-click pressed");
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {
+                        // println!("unhandled event {event:?}");
+                    }
+                })
                 .build(app)?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_tasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
-
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
-</script>
-
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="row">
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+    <h1>Todolator</h1>
+    <h2>Tasks:</h2>
+    <p>{{ tasks }}</p>
 
     <form class="row" @submit.prevent="greet">
       <input id="greet-input" v-model="name" placeholder="Enter a name..." />
@@ -36,6 +12,21 @@ async function greet() {
   </main>
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const greetMsg = ref("");
+const name = ref("");
+const tasks = ref([]);
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke("greet", { name: name.value });
+  tasks.value = await invoke("get_tasks");
+}
+</script>
+
 <style scoped>
 .logo.vite:hover {
   filter: drop-shadow(0 0 2em #747bff);
@@ -44,7 +35,6 @@ async function greet() {
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #249b73);
 }
-
 </style>
 <style>
 :root {
@@ -156,5 +146,4 @@ button {
     background-color: #0f0f0f69;
   }
 }
-
 </style>
