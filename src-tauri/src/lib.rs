e7f48f928b42f150da74716d7606101e7e93c3e7
Tackx <tack@vxn.dev>
2025-06-01 23:16:14 +0200

    Refactor, Parse timestamps from JSON

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,12 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fs;
 use tauri::{
     menu::{Menu, MenuItem},
     tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
     webview::WebviewWindowBuilder,
-    AppHandle, Manager,
+    App, AppHandle, Manager,
 };
 
 #[tauri::command]
@@ -13,9 +14,10 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Task {
     name: String,
+    timestamp: DateTime<Utc>,
 }
 
 #[tauri::command]
@@ -27,13 +29,51 @@ fn get_tasks() -> Result<Vec<Task>, String> {
     Ok(tasks)
 }
 
+fn setup_tray(app: &mut App) {
+    let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>).unwrap();
+    let menu = Menu::with_items(app, &[&quit_i]).unwrap();
+
+    let _tray = TrayIconBuilder::new()
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .icon(app.default_window_icon().unwrap().clone())
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => {
+                println!("quit menu item was clicked");
+                app.exit(0);
+            }
+            _ => {
+                println!("menu item {:?} not handled", event.id);
+            }
+        })
+        .on_tray_icon_event(|tray, event| match event {
+            TrayIconEvent::DoubleClick {
+                button: MouseButton::Left,
+                ..
+            } => {
+                println!("double left-click pressed");
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
+            _ => {
+                // println!("unhandled event {event:?}");
+            }
+        })
+        .build(app)
+        .unwrap();
+}
+
 fn spawn_reminder(handle: AppHandle) {
     std::thread::spawn(move || {
         WebviewWindowBuilder::new(
             &handle,
-            "label",
+            "reminder",
             tauri::WebviewUrl::App("reminder.html".into()),
         )
+        .title("Todolator Reminder")
         .build()
         .unwrap();
     });
@@ -43,42 +83,11 @@ fn spawn_reminder(handle: AppHandle) {
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            setup_tray(app);
+            // Spawn a new window for a pop-up reminder
             let handle = app.handle().clone();
             spawn_reminder(handle);
 
-            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i])?;
-
-            let _tray = TrayIconBuilder::new()
-                .menu(&menu)
-                .show_menu_on_left_click(false)
-                .icon(app.default_window_icon().unwrap().clone())
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => {
-                        println!("quit menu item was clicked");
-                        app.exit(0);
-                    }
-                    _ => {
-                        println!("menu item {:?} not handled", event.id);
-                    }
-                })
-                .on_tray_icon_event(|tray, event| match event {
-                    TrayIconEvent::DoubleClick {
-                        button: MouseButton::Left,
-                        ..
-                    } => {
-                        println!("double left-click pressed");
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                    }
-                    _ => {
-                        // println!("unhandled event {event:?}");
-                    }
-                })
-                .build(app)?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
