6702a38d91a51673718886925386b84f50eb721a
Tackx <tack@vxn.dev>
2025-07-05 19:33:41 +0200

    Style GUI, Add create modal

--- src/components/PTasks.vue
+++ src/components/PTasks.vue
@@ -1,28 +1,44 @@
 <template>
-    <main class="container">
-        <h1>Todolator</h1>
+    <!-- Modal overlay -->
+    <div class="min-w-screen w-screen min-h-screen opacity-50 bg-black z-1 fixed " v-if="displayNewTaskModal"></div>
 
-        <h2>Tasks:</h2>
-        <p>{{ tasks }}</p>
-
-        <form class="row" @submit.prevent="addTask">
-            <input id="greet-input" v-model="name" placeholder="Title" type="text" />
-            <input id="greet-input" v-model="desc" placeholder="Description" type="text" />
-            <input id="greet-input" v-model="timestamp" placeholder="Timestamp" type="datetime-local" />
-            <button type="submit">Add task</button>
-        </form>
-
+    <main class="flex flex-col items-center justify-center p-4">
+        <TaskForm submit-text="Save" :display="displayNewTaskModal"
+            @submit="displayNewTaskModal = !displayNewTaskModal" />
+        <h1 class="text-secondary">Todolator</h1>
+        <div class="flex flex-row w-full items-center justify-end">
+            <button @click="displayNewTaskModal = !displayNewTaskModal">New Task</button>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Timestamp</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="task in tasks">
+                    <td>{{ task.name }}</td>
+                    <td>{{ task.desc ? task.desc : '-' }}</td>
+                    <td>{{ task.timestamp }}</td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { nextTick, onMounted, onUnmounted, Ref, ref, watch } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import TaskForm from './TaskForm.vue'
 
-const tasks = ref();
-const name = ref();
-const desc = ref();
-const timestamp = ref();
+const tasks = ref() as Ref<Array<ITask>>;
+// const name = ref();
+// const desc = ref();
+// const timestamp = ref();
+
+const displayNewTaskModal = ref(false);
 
 try {
     tasks.value = await invoke("get_tasks");
@@ -33,26 +49,46 @@ try {
 
 console.log(tasks.value);
 
-async function addTask() {
-    try {
-        const task = { name: name.value, desc: desc.value, timestamp: new Date(timestamp.value).toISOString() };
+type DateTimeString = string;
 
-        console.log(`name: ${name.value}, description: ${desc.value}, timestamp: ${timestamp.value}`)
-
-        await invoke("create_task", task);
-        console.log("task created!");
-
-        // const newTasks = await invoke("get_tasks");
-        // console.log(`new tasks:`);
-        // console.log(newTasks);
-
-        tasks.value.push(task)
-    }
-    catch (e) {
-        console.log(e);
-    }
+interface ITask {
+    name: string;
+    desc?: string;
+    timestamp: DateTimeString;
 }
+
+// async function addTask() {
+//     try {
+//         const task = { name: name.value, desc: desc.value, timestamp: new Date(timestamp.value).toISOString() };
+
+//         console.log(`name: ${name.value}, description: ${desc.value}, timestamp: ${timestamp.value}`)
+
+//         await invoke("create_task", task);
+//         console.log("task created!");
+
+//         // const newTasks = await invoke("get_tasks");
+//         // console.log(`new tasks:`);
+//         // console.log(newTasks);
+
+//         tasks.value.push(task)
+//     }
+//     catch (e) {
+//         console.log(e);
+//     }
+// }
+
+onMounted(() => {
+    const handler = (e: KeyboardEvent) => {
+        if (e.key === "n") {
+            displayNewTaskModal.value = !displayNewTaskModal.value;
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    };
+    window.addEventListener("keydown", handler);
+    onUnmounted(() => window.removeEventListener("keydown", handler));
+});
 
 </script>
 
-<style scoped></style>
+<style></style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <div v-if="display"
+        class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-2  bg-primary p-10 rounded-md">
+        <h1 class="text-center">New Task</h1>
+        <form class="row flex flex-col justify-center items-center gap-2 mt-8" ref="form" @submit.prevent
+            @keydown.enter.ctrl="$emit('submit')">
+            <input v-model="name" placeholder="Title" type="text" class="w-full" tabindex="1" ref="focusInput" />
+            <input v-model="desc" placeholder="Description" type="text" class="w-full" tabindex="2" />
+            <input v-model="timestamp" placeholder="Timestamp" type="datetime-local" class="w-full" tabindex="3" />
+            <button class="mt-8 mb-0" @click="$emit('submit')" tabindex="4">{{ submitText }}</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { nextTick, ref, watch } from 'vue';
+
+const props = defineProps({
+    submitText: {
+        type: String,
+        default: null
+    },
+    name: {
+        type: String,
+        default: null
+    },
+    desc: {
+        type: String,
+        default: null
+    },
+    timestamp: {
+        type: String,
+        default: null
+    },
+    display: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const submitText = ref(props.submitText);
+const name = ref(props.name);
+const desc = ref(props.desc);
+const timestamp = ref(props.timestamp);
+
+defineEmits(["submit"]);
+
+const focusInput = ref<HTMLInputElement | null>(null);
+watch(() => props.display, (display) => {
+    if (display) {
+        nextTick(() => {
+            focusInput.value?.focus();
+        });
+    }
+});
+
+</script>
+
+<style></style>
