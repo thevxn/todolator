5b512d084e6e7670e17fc21119232898c1a82792
Tackx <tack@vxn.dev>
2025-07-05 19:55:41 +0200

    Dynamically generate hotkeys

--- src/components/PHotkeys.vue
+++ src/components/PHotkeys.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="absolute bottom-0 w-full flex flex-row items-center justify-center gap-4">
         <div>Hotkeys:</div>
-        <div><span class="font-semibold">N: </span>New Task</div>
-        <div><span class="font-semibold">Esc: </span>Close Modal</div>
-        <div><span class="font-semibold">Shift + Enter: </span>Save Task</div>
+        <div v-for="hotkey in hotkeys"><span class="font-semibold">{{ hotkey.shortcut }}: </span><span>{{
+            hotkey.description }}</span></div>
     </div>
 </template>
 
 <script lang="ts" setup>
+const hotkeys = [
+    {
+        shortcut: "N",
+        description: "New Task"
+    },
+    {
+        shortcut: "Esc",
+        description: "Close Modal"
+    },
+    {
+        shortcut: "Shift + Enter",
+        description: "Save Task"
+    },
+]
 
 </script>
 
