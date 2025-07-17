b78159e645a7b2c8c1c96c11286a050d78b41088
Tackx <tack@vxn.dev>
2025-07-17 11:21:21 +0200

    Adjust popup styles

--- src/components/ReminderPopup.vue
+++ src/components/ReminderPopup.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="flex flex-col w-full h-full items-center justify-center">
-    <div>
-      <h2 class="text-center">{{ task?.name }}</h2>
-      <p>
-        <span class="text-lg">{{ task?.desc }}</span>
-      </p>
-      <button @click="$emit('close', task)">Done</button>
-    </div>
+    <h2 class="text-center">
+      [{{ timestampDate().getHours().toString().padStart(2, '0') }}:{{
+        timestampDate().getMinutes().toString().padStart(2, '0')
+      }}]
+      {{ task?.name }}
+    </h2>
+    <p class="mb-2" v-if="task?.desc">
+      <span class="text-lg">{{ task?.desc }}</span>
+    </p>
+    <button @click="$emit('close', task)">Done</button>
     <PHotkeys screen-code="REMINDER_POPUP" />
   </div>
 </template>
@@ -18,6 +21,7 @@ import { invoke } from '@tauri-apps/api/core'
 import PHotkeys from './PHotkeys.vue'
 
 const task = ref<Task>()
+const timestampDate = () => new Date(task.value?.timestamp as string)
 
 try {
   task.value = await invoke('get_next_task')
